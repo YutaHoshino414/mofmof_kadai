e6e879a5d47d4613da1c1a1f4c9bfad9b5872b65
Yuta Hoshino <y.hoshino25@gmail.com>
2020-12-22 13:15:04 +0900

    モデル同士を外部キーで関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_040253) do
+ActiveRecord::Schema.define(version: 2020_12_22_041311) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_12_22_040253) do
     t.integer "walk_minute"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "property_id"
+    t.index ["property_id"], name: "index_stations_on_property_id"
   end
 
 end
